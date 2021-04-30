7da9819d15abf719bd6711b36e2c64ac0ecf8017
luozejian <luozejian@infosouth.com.cn>
2021-04-30 13:44:29 +0800

    doc(README.MD)支持js后缀的React组件

---
+++ src/tsx.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+let ccc = `测试文本`
+const Save = (props) => {
+    return (
+        <div testAttr={`测试属性${xxxx}${xxxx}`}>
+            {xxxx}
+        </div>
+    )
+}
+
+export default Save
+
